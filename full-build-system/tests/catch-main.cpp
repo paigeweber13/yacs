eff7fcb0b0de67f364f5914ac5acd1833044924e
Riley Weber <rileyw13@protonmail.com>
2021-02-12 17:10:22 -0500

    added full-build-system test files, this was an omission earlier! Oops!

---
+++ full-build-system/tests/catch-main.cpp
@@ -0,0 +1,5 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+TEST_CASE( "00: All test cases reside in other .cpp files (empty)", "[multi-file:1]" ) {
+}
---
+++ full-build-system/tests/test-01-hello.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch.hpp>
+
+#include "hello.hpp"
+
+TEST_CASE( "01: I get five.", "[hello]" ) {
+  REQUIRE( gimmeFive() == 5 );
+}
